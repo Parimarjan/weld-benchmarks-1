1ba9d51d6913458d5b495d1850969c08b67f66ec
Parimarjan Negi <pnegi@stanford.edu>
2017-12-09 08:01:27 -0800

    updated stuff for incremental runs

--- benchmarks/run_all.py
+++ benchmarks/run_all.py
@@ -2,13 +2,13 @@ import argparse
 import subprocess as sp
 import os
 
-def run_cmd(orig_cmd, name):
+def run_cmd(orig_cmd, name, only_end_to_end):
     '''
     -- For each cmd, will run it once with numpy and once with weld and output results to the same
     file.
     -- After process_outputs is done, will delete the file.
     '''
-    tries = 5
+    tries = 2
     for i in range(tries):
         # Keep this file for both numpy and weld so can process output easily.
         fname = name + str(i) + '.txt'
@@ -29,7 +29,8 @@ def run_cmd(orig_cmd, name):
         process.wait()
         f.close()
     # Let's run the dump-csv script and pass in name. Matches all files with similar name.
-    dump_cmd = 'python ./../process_outputs.py -f {f}'.format(f=name)
+    dump_cmd = 'python ./../process_outputs.py -f {f} -only_end_to_end {}'.format(f=name,
+            e=only_end_to_end)
     dump_cmd = dump_cmd.split()
     process = sp.Popen(dump_cmd)
     process.wait()
@@ -38,34 +39,34 @@ def run_cmd(orig_cmd, name):
         fname = name + str(i) + '.txt'
         os.remove(fname)
 
-def run_blackscholes(n, p, name):
+def run_blackscholes(n, p, name, only_end_to_end=False):
     os.chdir('blackscholes')
     f = 'bench'
     args = '-n {n} -ie 1 -g 0 -p {p}'.format(n=n, p=p)
     cmd = 'python {file} {args}'.format(file=f, args=args)
     cmd = cmd.split()
-    run_cmd(cmd, name)
+    run_cmd(cmd, name, only_end_to_end)
     os.chdir('..')
 
-def run_nbody(n, p, name):
+def run_nbody(n, p, name, only_end_to_end=False):
     os.chdir('nbody')
     f = 'nbody.py'
     args = '-n {n} -t 1 -p {p}'.format(n=n, p=p)
     cmd = 'python {file} {args}'.format(file=f, args=args)
     cmd = cmd.split()
-    run_cmd(cmd, name)
+    run_cmd(cmd, name, only_end_to_end)
     os.chdir('..')
 
-def run_haversine(s, p, name):
+def run_haversine(s, p, name, only_end_to_end=False):
     os.chdir('haversine')
     f = 'main.py'
     args = '-s {s} -g 1 -p {p}'.format(s=s, p=p)
     cmd = 'python {file} {args}'.format(file=f, args=args)
     cmd = cmd.split()
-    run_cmd(cmd, name)
+    run_cmd(cmd, name, only_end_to_end)
     os.chdir('..')
 
-def run_quasi(n, p, name):
+def run_quasi(n, p, name, only_end_to_end=False):
     os.chdir('quasicrystal')
     f = 'quasicrystal.py'
     # TODO: loop over different options here?
@@ -76,59 +77,66 @@ def run_quasi(n, p, name):
     args = '-k {k} -s {s} -n {n} -t {t} -p {p}'.format(k=k, s=s, t=t, n=n, p=p)
     cmd = 'python {file} {args}'.format(file=f, args=args)
     cmd = cmd.split()
-    run_cmd(cmd, name)
-    os.chdir('..')
-
-def run_haversine(s, p, name):
-    os.chdir('haversine')
-    f = 'main.py'
-    args = '-s {s} -g 1 -p {p}'.format(s=s, p=p)
-    cmd = 'python {file} {args}'.format(file=f, args=args)
-    cmd = cmd.split()
-    run_cmd(cmd, name)
+    run_cmd(cmd, name, only_end_to_end)
     os.chdir('..')
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--d", type=int, required=False,
                     default=1, help="divide all size args by d")
+parser.add_argument("-run_ablation", type=int, required=False,
+                    default=0, help="run ablation studies?")
+parser.add_argument("-run_incremental", type=int, required=False,
+                    default=0, help="run ablation studies?")
+
 args = parser.parse_args()
 
 # ~100 seconds for numpy
 BLACKSCHOLES_ARGS = (10**8)*2 /args.d
-run_blackscholes(BLACKSCHOLES_ARGS, 'whatever', 'blackscholes')
-# ablation studies!
-run_blackscholes(BLACKSCHOLES_ARGS, 'fusion', 'blackscholes')
-run_blackscholes(BLACKSCHOLES_ARGS, 'vector', 'blackscholes')
-run_blackscholes(BLACKSCHOLES_ARGS, 'infer', 'blackscholes')
+BLACKSCHOLES_SUPPORTED_OPS = [np.sqrt, np.divide, np.exp, np.add, np.subtract]
 
-# run_blackscholes(BLACKSCHOLES_ARGS/args.d, 'predicat', 'blackscholes_predicat')
-# run_blackscholes(BLACKSCHOLES_ARGS/args.d, 'circuit', 'blackscholes_circuit')
+run_blackscholes(BLACKSCHOLES_ARGS, 'whatever', 'blackscholes')
+
+if args.run_incremental:
+    # start with no supported ops, and then successively keep adding supported ops.
+
+# ablation studies!
+if args.run_ablation:
+    run_blackscholes(BLACKSCHOLES_ARGS, 'fusion', 'blackscholes')
+    run_blackscholes(BLACKSCHOLES_ARGS, 'vector', 'blackscholes')
+    run_blackscholes(BLACKSCHOLES_ARGS, 'infer', 'blackscholes')
+    run_blackscholes(BLACKSCHOLES_ARGS/args.d, 'predicat', 'blackscholes_predicat')
+    run_blackscholes(BLACKSCHOLES_ARGS/args.d, 'circuit', 'blackscholes_circuit')
 
 # gives it 50-60 secs as we want.
 HAVERSINE_SCALE = 10**5 / args.d
 run_haversine(HAVERSINE_SCALE, 'whatever', 'haversine')
-# ablation studies!
-run_haversine(HAVERSINE_SCALE, 'fusion', 'haversine')
-run_haversine(HAVERSINE_SCALE, 'vector', 'haversine')
-run_haversine(HAVERSINE_SCALE, 'infer', 'haversine')
-# run_haversine(HAVERSINE_SCALE, 'predicate', 'haversine_predicate')
-# run_haversine(HAVERSINE_SCALE, 'circuit', 'haversine_circuit')
+
+if args.run_ablation:
+    run_haversine(HAVERSINE_SCALE, 'fusion', 'haversine')
+    run_haversine(HAVERSINE_SCALE, 'vector', 'haversine')
+    run_haversine(HAVERSINE_SCALE, 'infer', 'haversine')
+    run_haversine(HAVERSINE_SCALE, 'predicate', 'haversine_predicate')
+    run_haversine(HAVERSINE_SCALE, 'circuit', 'haversine_circuit')
 
 # Keeps it around ~70 seconds for numpy
 NBODY_ARGS = 25000 / args.d
+
 run_nbody(NBODY_ARGS, 'whatever', 'nbody')
-run_nbody(NBODY_ARGS, 'fusion', 'nbody')
-run_nbody(NBODY_ARGS, 'vector', 'nbody')
-run_nbody(NBODY_ARGS, 'infer', 'nbody')
-# run_nbody(NBODY_ARGS, 'predicate', 'nbody_predicate')
-# run_nbody(NBODY_ARGS, 'circuit', 'nbody_circuit')
+
+if args.run_ablation:
+    run_nbody(NBODY_ARGS, 'fusion', 'nbody')
+    run_nbody(NBODY_ARGS, 'vector', 'nbody')
+    run_nbody(NBODY_ARGS, 'infer', 'nbody')
+    run_nbody(NBODY_ARGS, 'predicate', 'nbody_predicate')
+    run_nbody(NBODY_ARGS, 'circuit', 'nbody_circuit')
 
 PIXELS = 2048 / args.d
 run_quasi(PIXELS, 'whatever', 'quasi')
-run_quasi(PIXELS, 'fusion', 'quasi')
-run_quasi(PIXELS, 'vector', 'quasi')
-run_quasi(PIXELS, 'infer', 'quasi')
 
-# run_quasi('predicate', 'quasi')
-# run_quasi('circuit', 'quasi')
+if args.run_ablation:
+    run_quasi(PIXELS, 'fusion', 'quasi')
+    run_quasi(PIXELS, 'vector', 'quasi')
+    run_quasi(PIXELS, 'infer', 'quasi')
+    run_quasi('predicate', 'quasi')
+    run_quasi('circuit', 'quasi')
